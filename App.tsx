04db73c772adbcce6f4c8d0945b4c42a958dad07
Andre Luiz <andreluizgsantos01@gmail.com>
2023-11-02 12:16:09 -0300

    Testando

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Turismo ES</Text>
+      <Text>Turismo ES </Text>
       <StatusBar style="auto" />
     </View>
   );
